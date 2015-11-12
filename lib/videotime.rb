34cd3b326416277c0001760fb2baad17c564e5ac
Akshay <akshay.goyal1008@gmail.com>
2015-11-12 15:13:27 +0530

    initial commit

---
+++ lib/videotime.rb
@@ -0,0 +1,9 @@
+require "videotime/version"
+
+module Videotime
+  def self.validate_video_time(path)
+	result = `ffmpeg -i #{path} 2>&1`
+    r = result.match("Duration: ([0-9]+):([0-9]+):([0-9]+).([0-9]+)")
+    duration = r[1].to_i*3600+r[2].to_i*60+r[3].to_i if r
+  end
+end
---
+++ lib/videotime/version.rb
@@ -0,0 +1,3 @@
+module Videotime
+  VERSION = "0.0.1"
+end
